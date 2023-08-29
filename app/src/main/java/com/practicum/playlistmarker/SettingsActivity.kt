2b6ddf235c5712a6a57571864b67db387374b612
Evlenium <evlenium@mail.ru>
2023-08-29 14:04:54 +0300

    Corrected activity screens,dimens and add finish function

--- app/src/main/java/com/practicum/playlistmarker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmarker/SettingsActivity.kt
@@ -1,6 +1,5 @@
 package com.practicum.playlistmarker
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
@@ -9,11 +8,9 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-
         val buttonBackToMainFromSettings = findViewById<ImageButton>(R.id.buttonBackToMainFromSettings)
         buttonBackToMainFromSettings.setOnClickListener{
-            val buttonBackToMainFromSettingsIntent = Intent(this,MainActivity::class.java)
-            startActivity(buttonBackToMainFromSettingsIntent)
+            finish()
         }
     }
 }
