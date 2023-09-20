425d20983a8d530a2876fd9b2768b4b6b9db8b77
Evlenium <evlenium@mail.ru>
2023-09-20 20:39:15 +0300

    Added dark theme,settings activity,search and primitive memory

--- app/src/main/java/com/practicum/playlistmarker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmarker/SearchActivity.kt
@@ -1,11 +1,71 @@
 package com.practicum.playlistmarker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+    companion object {
+        const val INPUT_TEXT_SEARCH = "INPUT_TEXT_SEARCH"
+    }
+    lateinit var inputTextFromSearch: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val buttonBackToMainFromSearch =
+            findViewById<ImageButton>(R.id.buttonBackToMainFromSearch)
+        buttonBackToMainFromSearch.setOnClickListener {
+            finish()
+        }
+
+        val inputEditTextSearch = findViewById<EditText>(R.id.inputEditTextSearch)
+        val clearTextSearchIcon = findViewById<ImageView>(R.id.clearTextSearchIcon)
+
+        clearTextSearchIcon.setOnClickListener {
+            inputEditTextSearch.setText("")
+            this.currentFocus?.let { view ->
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+                imm?.hideSoftInputFromWindow(view.windowToken, 0)
+            }
+        }
+
+        val searchTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(s!=null){
+                inputTextFromSearch = s.toString()
+                }
+                clearTextSearchIcon.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        inputEditTextSearch.addTextChangedListener(searchTextWatcher)
+    }
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(INPUT_TEXT_SEARCH,inputTextFromSearch)
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        inputTextFromSearch = savedInstanceState.getString(INPUT_TEXT_SEARCH,"")
     }
 }
--- app/src/main/java/com/practicum/playlistmarker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmarker/SettingsActivity.kt
@@ -1,16 +1,73 @@
 package com.practicum.playlistmarker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ShareCompat
+
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        val buttonBackToMainFromSettings = findViewById<ImageButton>(R.id.buttonBackToMainFromSettings)
-        buttonBackToMainFromSettings.setOnClickListener{
+        val buttonBackToMainFromSettings =
+            findViewById<ImageButton>(R.id.buttonBackToMainFromSettings)
+        buttonBackToMainFromSettings.setOnClickListener {
             finish()
+        }
+
+        val buttonShareApplication = findViewById<ImageButton>(R.id.buttonShareApplication)
+        buttonShareApplication.setOnClickListener {
+            val shareText = resources.getString(R.string.button_share_application_text)
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, shareText)
+                type = "text/plain"
+            }
+            startActivity(sendIntent)
+        }
+
+        val buttonSupport = findViewById<ImageButton>(R.id.buttonSupport)
+        buttonSupport.setOnClickListener {
+//            val email = resources.getString(R.string.support_mail)
+//            val subject = resources.getString(R.string.support_subject)
+//            val body = resources.getString(R.string.support_body)
+//            ShareCompat.IntentBuilder.from(this)
+//                .setType("message/rfc822")
+//                .addEmailTo(email)
+//                .setSubject(subject)
+//                .setText(body)
+//                .startChooser()
+
+//            val address = resources.getString(R.string.support_mail)
+//            val subject = resources.getString(R.string.support_subject)
+//            val body = resources.getString(R.string.support_body)
+//            val intent = Intent(Intent.ACTION_SENDTO)
+//            intent.data = Uri.parse("mailto:$address")
+//            //intent.putExtra(Intent.EXTRA_EMAIL, address)
+//            intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+//            intent.putExtra(Intent.EXTRA_TEXT, body)
+//            startActivity(intent)
+
+            val email = resources.getString(R.string.support_mail)
+            val subject = resources.getString(R.string.support_subject)
+            val body = resources.getString(R.string.support_body)
+            val uri: Uri = Uri.parse("mailto:")
+                .buildUpon()
+                .appendQueryParameter("to",email)
+                .appendQueryParameter("subject",subject)
+                .appendQueryParameter("body", body)
+                .build()
+            val emailIntent = Intent(Intent.ACTION_SENDTO,uri)
+            startActivity(Intent.createChooser(emailIntent,"subject"))
+        }
+
+        val buttonTermsOfUse = findViewById<ImageButton>(R.id.buttonTermsOfUse)
+        buttonTermsOfUse.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(resources.getString(R.string.YP_offer_url)))
+            startActivity(browserIntent)
         }
     }
 }
